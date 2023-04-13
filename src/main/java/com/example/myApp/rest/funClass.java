809dc70e7b0e0592dfdefac8929e35b844b52a91
safranekmart <120668643+safranekmart@users.noreply.github.com>
2023-04-13 14:02:34 +0200

    Update funClass.java
    Přidání prvního node

--- src/main/java/com/example/myApp/rest/funClass.java
+++ src/main/java/com/example/myApp/rest/funClass.java
@@ -1,4 +1,13 @@
 package com.example.myApp.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class funClass {
+
+    @GetMapping("/")
+    public String pozdrav() {
+        return "Ahoj, jak se máš?!";
+    }
 }
